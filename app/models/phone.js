ccbf6511699394ad9b986207cbdb94a950e96f78
Jake Craige <jake@poeticsystems.com>
2014-04-16 23:29:33 -0500

    use fixture adapter for phone data

---
+++ app/models/phone.js
@@ -0,0 +1,32 @@
+import PhoneFixtures from 'ember-phonecat/models/phone-fixtures';
+
+var attr = DS.attr
+
+var Phone =  DS.Model.extend({
+  age: attr(),
+  imageUrl: attr(),
+  name: attr(),
+  snippet: attr(),
+  additionalFeatures: attr(),
+  android: attr(),
+  availability: attr(),
+  battery: attr(),
+  camera: attr(),
+  connectivity: attr(),
+  description: attr(),
+  display: attr(),
+  hardware: attr(),
+  images: attr(),
+  sizeAndWeight: attr(),
+  storage: attr(),
+
+  cameraJoinedFeatures: function() {
+    return this.get('camera.features').join(', ');
+  }.property('camera.features.[]')
+});
+
+Phone.reopenClass({
+  FIXTURES: PhoneFixtures()
+});
+
+export default Phone;
